c2f3530a938c759a39ac9e79ca325fb95cce971d
vaishnavidumbare05 <vaishnavidumbare2004@gmail.com>
2024-07-27 13:09:55 +0530

    using key of loop

--- example1.js
+++ example1.js
@@ -3,10 +3,19 @@
 const numbers=[55,29,94,28,35,99]
 
 largestNum=numbers[0]
-for(let i=0;i<numbers.length;i++)
-    {
-      if(numbers[i]>largestNum){
-        largestNum=numbers[i]
-      }
-}
-console.log(`largest number in array is ${largestNum}`)
+// for(let i=0;i<numbers.length;i++)
+//     {
+//       if(numbers[i]>largestNum){
+//         largestNum=numbers[i]
+//       }
+// }
+
+//using key of loop
+   for(const num of numbers)
+        {
+      if(num>largestNum){
+            largestNum=num
+          }
+       }
+
+console.log(`Largest number in array is ${largestNum}`)
