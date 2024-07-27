a9b11b795cae7808783f2c555db9825c965277bf
vaishnavidumbare05 <vaishnavidumbare2004@gmail.com>
2024-07-27 13:21:37 +0530

    sum of odd number using key of loop

--- example2.js
+++ example2.js
@@ -3,11 +3,17 @@
 const numbers=[4,10,67,36,55,13]
 
 let sum=0;
-for(let i=0;i<numbers.length;i++)
-{
-    if(numbers[i]%2==1){
-        sum += numbers[i];
-    }
+// for(let i=0;i<numbers.length;i++)
+// {
+//     if(numbers[i]%2==1){
+//         sum += numbers[i];
+//     }
    
-}
+// }
+
+ for(const num of numbers){
+    if(num%2==1){
+        sum += num
+    }
+ }
 console.log(`Sum of odd number in array is ${sum}`);
